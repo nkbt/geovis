da1dfea3af7ab973e6df9f02f7f2d81a002ac74b
Nik Butenko <nik@butenko.me>
2016-08-18 09:19:14 +1000

    Skip extra extensions

--- test/index.js
+++ test/index.js
@@ -1,4 +1,11 @@
 'use strict';
 
+const noop = () => null;
+
+// Skip all extra extensions
+require.extensions['.jpg'] = noop;
+require.extensions['.jpeg'] = noop;
+require.extensions['.png'] = noop;
+require.extensions['.svg'] = noop;
 
 require('react-component-template/test');
