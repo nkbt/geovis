a64046d7aa8e3a18d2a240f204c6c0f09a1ffa5a
Nik Butenko <nik@butenko.me>
2016-09-09 08:31:43 +1000

    Simplify controls

--- src/Controls.js
+++ src/Controls.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {shouldComponentUpdate} from 'react/lib/ReactComponentWithPureRenderMixin';
 import {connect} from 'react-redux';
 import {themr} from 'react-css-themr';
-import {ATTACKS_ADD_RANDOM, ATTACKS_CLEAR} from './attacks/reducer';
 import {
   STATE_PAUSED,
   STATE_PLAYING,
@@ -13,9 +12,8 @@ import {
 
 const css = {
   controlsContainer: 'GeoVis--Controls--container',
-  controlsOpened: 'GeoVis--Controls--opened',
   controlsContent: 'GeoVis--Controls--content',
-  controlsToggle: 'GeoVis--Controls--toggle'
+  controlsButton: 'GeoVis--Controls--button'
 };
 
 
@@ -23,41 +21,24 @@ const ControlsContent = React.createClass({
   propTypes: {
     theme: React.PropTypes.object,
     controls: React.PropTypes.string.isRequired,
-    onAdd: React.PropTypes.func.isRequired,
-    onClear: React.PropTypes.func.isRequired,
     onPlay: React.PropTypes.func.isRequired,
     onPause: React.PropTypes.func.isRequired
-  },
-
-
-  getInitialState() {
-    return {isOpened: false};
   },
 
 
   shouldComponentUpdate,
 
 
-  onToggle(event) {
-    event.preventDefault();
-    this.setState({isOpened: !this.state.isOpened});
-  },
-
-
   render() {
-    const {theme, controls, onAdd, onClear, onPlay, onPause} = this.props;
-    const {isOpened} = this.state;
+    const {theme, controls, onPlay, onPause} = this.props;
 
     return (
-      <div className={`${theme.controlsContainer} ${isOpened ? theme.controlsOpened : ''}`}>
+      <div className={theme.controlsContainer}>
         <div className={theme.controlsContent}>
-          <span className={theme.controlsToggle} onClick={this.onToggle}>
-            {isOpened ? '>' : '<'}
-          </span>
-          {controls === STATE_PLAYING ? <button onClick={onPause}>Pause</button> : null}
-          {controls === STATE_PAUSED ? <button onClick={onPlay}>Play</button> : null}
-          <button onClick={onAdd}>Add</button>
-          <button onClick={onClear}>Clear</button>
+          {controls === STATE_PLAYING ?
+            <button className={theme.controlsButton} onClick={onPause}>Pause</button> : null}
+          {controls === STATE_PAUSED ?
+            <button className={theme.controlsButton} onClick={onPlay}>Play</button> : null}
         </div>
       </div>
     );
@@ -73,8 +54,6 @@ const mapStateToProps = ({
 
 
 const mapDispatchToProps = dispatch => ({
-  onAdd: () => dispatch({type: ATTACKS_ADD_RANDOM}),
-  onClear: () => dispatch({type: ATTACKS_CLEAR}),
   onPause: () => dispatch({type: CONTROLS_PAUSE}),
   onPlay: () => dispatch({type: CONTROLS_PLAY})
 });
