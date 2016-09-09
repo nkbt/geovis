fca8ee54d14b950c80f0059e08de2375780c590f
Nik Butenko <nik@butenko.me>
2016-09-09 12:43:22 +1000

    Export all all reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import * as attacksReducer from './attacks/reducer';
+import * as controlsReducer from './controls/reducer';
+
 export {App} from './App';
 export {Adder} from './Adder';
 export {Remover} from './Remover';
 export {Controls} from './Controls';
 export {InvisibleControls} from './InvisibleControls';
 export {Globe} from './Globe';
-export {attacks} from './attacks/reducer';
-export {controls} from './controls/reducer';
+export const attacks = attacksReducer;
+export const controls = controlsReducer;
