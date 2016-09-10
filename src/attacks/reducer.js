d37903d13c41e8ad93735afb2ce7b99c2a19f855
Nik Butenko <nik@butenko.me>
2016-09-10 14:09:42 +1000

    Better randomize

--- src/attacks/reducer.js
+++ src/attacks/reducer.js
@@ -3,19 +3,39 @@ import {rnd} from '../lib/utils';
 const SYD = [-33.865143, 151.209900];
 const DARWIN = [-12.462827, 130.841782];
 const NY = [40.730610, -73.935242];
+const SF = [38.006811, -122.667424];
 const LONDON = [51.509865, -0.118092];
 const VANCOUVER = [49.246292, -123.116226];
 const MOSCOW = [55.751244, 37.618423];
 const KYIV = [50.411198, 30.446634];
-const points = [
+const TOKYO = [35.717243, 19.761646];
+const MUMBAI = [19.064499, 72.919112];
+const MADRID = [40.395906, -3.581182];
+const DUBLIN = [53.464008, -6.218773];
+const MIAMI = [25.669930, -80.126130];
+const LIMA = [-12.126830, -77.042177];
+const BUENOS_AIRES = [-34.309534, -58.700775];
+
+
+const cities = [
   SYD,
   DARWIN,
   NY,
+  SF,
   LONDON,
   VANCOUVER,
   MOSCOW,
-  KYIV
+  KYIV,
+  TOKYO,
+  MUMBAI,
+  MADRID,
+  DUBLIN,
+  MIAMI,
+  LIMA,
+  BUENOS_AIRES
 ];
+
+
 const colors = [
   0x33ff33,
   0xffff33,
@@ -25,9 +45,14 @@ const colors = [
 
 const sample = arr => arr[Math.round(Math.random() * (arr.length - 1))];
 
-export const mkAttack = ([srcLat, srcLon], [dstLat, dstLon]) => ({
-  id: `${srcLat}|${srcLon}|${dstLat}|${dstLon}|${performance.now()}`,
-  srcLat, srcLon, dstLat, dstLon, value: rnd(5, 30), color: sample(colors)
+const mkAttack = (src, dst) => ({
+  id: `${cities.indexOf(src)}|${cities.indexOf(dst)}`,
+  srcLat: src[0],
+  srcLon: src[1],
+  dstLat: dst[0],
+  dstLon: dst[1],
+  value: rnd(1, 10),
+  color: sample(colors)
 });
 
 
@@ -52,7 +77,14 @@ export const remove = (state, {ids = []}) => {
 };
 
 
-const addRandom = state => add(state, {attacks: [mkAttack(sample(points), sample(points))]});
+const addRandom = state => {
+  const src = sample(cities);
+  const other = [].concat(cities);
+  other.splice(cities.indexOf(src), 1);
+  const dst = sample(other);
+
+  return add(state, {attacks: [mkAttack(src, dst)]});
+};
 
 
 const initialState = {};
